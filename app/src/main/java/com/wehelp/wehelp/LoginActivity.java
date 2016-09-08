e3ce6e531760db05fef64017869bb9034c27b9f9
Thomas Pessato <thomaspessato@MacBook-Pro-de-Thomas.local>
2016-09-07 23:34:00 -0300

    [+] tabbed activity / timeline row

--- app/src/main/java/com/wehelp/wehelp/LoginActivity.java
+++ app/src/main/java/com/wehelp/wehelp/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.wehelp.wehelp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +18,14 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
+        Button loginBtn = (Button)findViewById(R.id.btn_login);
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, TabbedActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

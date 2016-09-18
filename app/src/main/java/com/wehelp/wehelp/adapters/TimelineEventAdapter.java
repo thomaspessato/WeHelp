d08a5d1c85bc7a59b26af9189194a2dfdccb9654
temp <temp@MacBook-Air-de-temp.local>
2016-09-18 17:10:50 -0300

    [+] more activities

--- app/src/main/java/com/wehelp/wehelp/adapters/TimelineEventAdapter.java
+++ app/src/main/java/com/wehelp/wehelp/adapters/TimelineEventAdapter.java
@@ -1,11 +1,15 @@
 package com.wehelp.wehelp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.google.android.gms.vision.text.Text;
+import com.wehelp.wehelp.EventDetailActivity;
 import com.wehelp.wehelp.R;
 import com.wehelp.wehelp.classes.Event;
 
@@ -15,8 +19,13 @@ import java.util.List;
  * Created by temp on 9/15/16.
  */
 public class TimelineEventAdapter extends ArrayAdapter<Event>{
+
+    private Context context;
+
+
     public TimelineEventAdapter(Context context, List<Event> list) {
         super(context, R.layout.row_timeline, list);
+        this.context = context;
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -26,6 +35,16 @@ public class TimelineEventAdapter extends ArrayAdapter<Event>{
             convertView = inflater.inflate(R.layout.row_timeline, null);
             System.out.println("timeline event: converTview was NULL");
         }
+
+        TextView linkInfoEvent = (TextView) convertView.findViewById(R.id.btn_moreinfo);
+
+        linkInfoEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), EventDetailActivity.class);
+                context.startActivity(intent);
+            }
+        });
 
 
         System.out.println("IM INSIDE GET VIEW, TIMELINE EVENTS!");

7452b55249fcd12907b00a89cbf1c01e93f1ff31
Thomas Pessato <tpessato@gmail.com>
2016-10-09 22:03:38 -0300

    [+] user location google maps

--- app/src/main/java/com/wehelp/wehelp/TabbedActivity.java
+++ app/src/main/java/com/wehelp/wehelp/TabbedActivity.java
@@ -1,8 +1,14 @@
 package com.wehelp.wehelp;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -19,6 +25,9 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.wehelp.wehelp.tabs.FragmentMap;
 import com.wehelp.wehelp.tabs.FragmentMyEvents;
 import com.wehelp.wehelp.tabs.FragmentTimeline;
@@ -39,11 +48,13 @@ public class TabbedActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    public GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabbed);
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
